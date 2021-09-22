ac97c5dade41fd5ddd3b2d7abbfeb287d29e9e66
Zahi Farah <zahi.farah@protonmail.com>
2021-09-22 19:56:50 +0300

    Update isValidEmail() regex formula.

--- script.js
+++ script.js
@@ -22,8 +22,9 @@ function showSuccess(input) {
 
 // Check Email
 function isValidEmail(email) {
-  const re = /\S+@\S+\.\S+/;
-  return re.test(email);
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 // Event listeners
