ff00e12ab3baa76c9319e7afa1e31e2f8fbe9a82
Zahi Farah <zahi.farah@protonmail.com>
2021-09-23 11:22:26 +0300

    Refactor email validation function, now called checkEmail()

--- script.js
+++ script.js
@@ -21,10 +21,15 @@ function showSuccess(input) {
 }
 
 // Check Email
-function isValidEmail(email) {
+function checkEmail(input) {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
 }
 
 // Check required fields
@@ -38,17 +43,31 @@ function checkRequired(inputArr) {
   });
 }
 
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-
-  // const inputArr = input.id.split("");
-  // const firstInputLetter = inputArr[0].toUpperCase();
-  // return firstInputLetter + input.id.slice(1);
 }
 
 // Event listeners
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
 });
